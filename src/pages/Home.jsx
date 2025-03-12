c732460b7ccbd87de20640a7470cd69133643fa2
MickEnev <mickenev@gmail.com>
2025-03-12 18:12:51 -0500

    search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MovieCard from '../components/MovieCard'
 
 export default function Home() {
+    const [searchQuery, setSearchQuery] = useState("")
+
     const movies = [
         {id: 1, title: "Pirates", release_date: "2009"},
         {id: 2, title: "Ninjas", release_date: "2019"},
         {id: 3, title: "Tigers", release_date: "2017"},
     ]
 
-    const handleSearch = () => {};
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+    };
 
   return (
     <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type='text' placeholder='Search movies...' className='search-input'/>
+            <input type='text' placeholder='Search movies...' className='search-input' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
             <button type="submit" className="search-button">Search</button>
         </form>
 
         <div className="movies-grid">
             {movies.map(movie => (
-            <MovieCard movie={movie} key={movie.id}/>
-            ))}
+                movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id}/>)
+            )}
         </div>
     </div>
   )
