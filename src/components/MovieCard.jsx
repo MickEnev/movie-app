40c57f631af9f7d0a2bcc68f3cb4260ca71d299e
Mick Enev <menev2@illinois.edu>
2025-03-14 12:05:34 -0500

    fixed uh oh

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,14 +10,14 @@ export default function ({movie}) {
   return (
     <div className="movieCard">
         <div className="movie-poster">
-            <img src={movie.url} alt={movie.title}/>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
                 <button className="favorite-btn" onClick={onLike}> :3 </button>
             </div>
         </div>
         <div className="movie-info">
             <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
+            <p>{movie.release_date?.split("-")[0]}</p>
         </div>
     </div>
   )
